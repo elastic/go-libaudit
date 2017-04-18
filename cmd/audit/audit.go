41bc5f50ffd30371af962caf3880ae8b7dab3f78
Andrew Kroh <andrew.kroh@elastic.co>
2017-04-18 11:36:11 -0400

    Update audit example to use Reassembler and aucoalesce

--- cmd/audit/audit.go
+++ cmd/audit/audit.go
@@ -30,14 +30,17 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/elastic/go-libaudit"
+	"github.com/elastic/go-libaudit/aucoalesce"
 	"github.com/elastic/go-libaudit/auparse"
 )
 
 var (
-	fs     = flag.NewFlagSet("audit", flag.ExitOnError)
-	debug  = fs.Bool("d", false, "enable debug output to stderr")
-	diag   = fs.String("diag", "", "dump raw information from kernel to file")
-	format = fs.String("format", "", "output format, possible values - json")
+	fs      = flag.NewFlagSet("audit", flag.ExitOnError)
+	debug   = fs.Bool("d", false, "enable debug output to stderr")
+	rate    = fs.Uint("rate", 0, "rate limit")
+	backlog = fs.Uint("backlog", 8192, "backlog limit")
+	diag    = fs.String("diag", "", "dump raw information from kernel to file")
+	format  = fs.String("format", "", "output format, possible values - json, coalesce")
 )
 
 func enableLogger() {
@@ -98,10 +101,30 @@ func read() error {
 		}
 	}
 
+	if status.RateLimit != uint32(*rate) {
+		log.Debugf("setting rate limit in kernel to %v", *rate)
+		if err = client.SetRateLimit(uint32(*rate), libaudit.NoWait); err != nil {
+			return errors.Wrap(err, "failed to set rate limit to unlimited")
+		}
+	}
+
+	if status.BacklogLimit != uint32(*backlog) {
+		log.Debugf("setting backlog limit in kernel to %v", *backlog)
+		if err = client.SetBacklogLimit(uint32(*backlog), libaudit.NoWait); err != nil {
+			return errors.Wrap(err, "failed to set backlog limit")
+		}
+	}
+
 	log.Debugf("sending message to kernel registering our PID (%v) as the audit daemon", os.Getpid())
 	if err = client.SetPID(libaudit.NoWait); err != nil {
 		return errors.Wrap(err, "failed to set audit PID")
 	}
+
+	reassembler, err := libaudit.NewReassembler(5, 2*time.Second, &streamHandler{})
+	if err != nil {
+		return errors.Wrap(err, "failed to create reassmbler")
+	}
+	defer reassembler.Close()
 
 	for {
 		rawEvent, err := client.Receive(false)
@@ -117,34 +140,51 @@ func read() error {
 			continue
 		}
 
-		// Ignore AUDIT_EOE.
-		if typ == auparse.AUDIT_EOE {
-			continue
-		}
-
-		if err := outputEvent(rawEvent); err != nil {
-			log.WithError(err).Warn("failed to output")
+		if err := reassembler.Push(rawEvent.MessageType, rawEvent.RawData); err != nil {
+			log.WithError(err).
+				WithField("type", typ).
+				WithField("raw_data", string(rawEvent.RawData)).
+				Warn("failed to push event to reassembler")
 		}
 	}
-
-	return nil
 }
 
-func outputEvent(raw *libaudit.RawAuditMessage) error {
-	typ := auparse.AuditMessageType(raw.MessageType)
-	msg := string(raw.RawData)
+type streamHandler struct{}
 
+func (s *streamHandler) ReassemblyComplete(msgs []*auparse.AuditMessage) {
 	switch *format {
 	default:
-		fmt.Printf("type=%v msg=%v\n", typ.String(), msg)
-	case "json":
-		auditMsg, _ := auparse.Parse(typ, msg)
-		jsonBytes, err := json.Marshal(auditMsg.ToMapStr())
-		if err != nil {
-			return err
+		for _, m := range msgs {
+			fmt.Printf("type=%v msg=%v\n", m.RecordType.String(), m.RawData)
 		}
-		fmt.Println(string(jsonBytes))
-	}
+	case "json":
+		for _, m := range msgs {
+			if err := printJSON(m.ToMapStr()); err != nil {
+				log.WithError(err).Error("failed to marshal message to JSON")
+			}
+		}
+	case "coalesce", "c":
+		event, err := aucoalesce.CoalesceMessages(msgs)
+		if err != nil {
+			log.WithError(err).Warn("failed to coalesce messages")
+			return
+		}
 
+		if err := printJSON(event); err != nil {
+			log.WithError(err).Error("failed to marshal event to JSON")
+		}
+	}
+}
+
+func (s *streamHandler) EventsLost(count int) {
+	log.Infof("Detected the loss of %v sequences.", count)
+}
+
+func printJSON(v interface{}) error {
+	jsonBytes, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(jsonBytes))
 	return nil
 }

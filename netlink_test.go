93a585d167959a101472c4e10a2e0dc6aa0d9fae
Andrew Kroh <andrew.kroh@elastic.co>
2017-06-08 16:02:56 -0400

    Add GetRules, DeleteRules, DeleteRule, AddRule to AuditClient
    Remove NetlinkErrno because the kernel doesn't use any of those error codes.

--- netlink_test.go
+++ netlink_test.go
@@ -17,8 +17,6 @@
 package libaudit
 
 import (
-	"bytes"
-	"encoding/binary"
 	"os"
 	"syscall"
 	"testing"
@@ -28,16 +26,6 @@ import (
 
 // Validate NetlinkClient implements NetlinkSendReceiver.
 var _ NetlinkSendReceiver = &NetlinkClient{}
-
-func TestParseNetlinkErrorDataTooShort(t *testing.T) {
-	assert.Error(t, ParseNetlinkError(nil), "too short")
-}
-
-func TestParseNetlinkErrorErrno(t *testing.T) {
-	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, -1*int32(NLE_MSG_TOOSHORT))
-	assert.Equal(t, ParseNetlinkError(buf.Bytes()), NLE_MSG_TOOSHORT)
-}
 
 func TestNewNetlinkClient(t *testing.T) {
 	c, err := NewNetlinkClient(syscall.NETLINK_AUDIT, nil, nil)

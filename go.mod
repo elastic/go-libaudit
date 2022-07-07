2c9040406df7a88ceac143334950438c653be7ee
Andrew Kroh <andrew.kroh@elastic.co>
2022-07-07 15:42:21 -0400

    Use go-shellquote to split rules into arguments
    The rule implementation naively split rules using strings.Fields, but this did not
    handle arguments that were quoted in cases where the arguments themselves
    contain spaces.
    
    Now go-shellquote is used to split the arguments before parsing.
    
    Fixes #114

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/elastic/go-libaudit/v2
 go 1.16
 
 require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/stretchr/testify v1.7.0
 	go.uber.org/multierr v1.7.0
 	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1
--- rule/gen_testdata_test.go
+++ rule/gen_testdata_test.go
@@ -30,10 +30,10 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"strings"
 	"testing"
 
+	"github.com/kballard/go-shellquote"
 	"gopkg.in/yaml.v2"
 
 	"github.com/elastic/go-libaudit/v2"
@@ -147,8 +147,7 @@ func auditctlExec(t testing.TB, command string) (string, []byte) {
 	// Replace paths with ones in a temp dir for test environment consistency.
 	command = makePaths(t, tempDir, command)
 
-	args := strings.Fields(command)
-	_, err = exec.Command("auditctl", args...).Output()
+	_, err = exec.Command("sh", "-c", "auditctl "+command).Output()
 	if err != nil {
 		var exitErr *exec.ExitError
 		if errors.As(err, &exitErr) {
@@ -174,31 +173,51 @@ func auditctlExec(t testing.TB, command string) (string, []byte) {
 // created for the test. It returns the updated command that contains the test
 // paths.
 func makePaths(t testing.TB, tmpDir, rule string) string {
-	re := regexp.MustCompile(`(-w |dir=|path=)/(\S+)`)
-	matches := re.FindAllStringSubmatch(rule, -1)
-	for _, match := range matches {
-		path := match[2]
-		realPath := filepath.Join(tmpDir, path)
+	args, err := shellquote.Split(rule)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i, arg := range args {
+		var prefix, path string
+		if arg == "-w" {
+			path = args[i+1]
+		} else if strings.HasPrefix(arg, "dir=") {
+			prefix = "dir="
+			path = strings.TrimPrefix(arg, prefix)
+		} else if strings.HasPrefix(arg, "path=") {
+			prefix = "path="
+			path = strings.TrimPrefix(arg, prefix)
+		} else {
+			continue
+		}
+
+		testPath := filepath.Join(tmpDir, path)
 
 		if strings.HasSuffix(path, "/") {
 			// Treat paths with trailing slashes as a directory to monitor.
-			if err := os.MkdirAll(realPath, 0o700); err != nil {
+			if err := os.MkdirAll(testPath, 0o700); err != nil {
 				t.Fatal(err)
 			}
 		} else {
 			// Touch a file.
-			dir := filepath.Dir(realPath)
+			dir := filepath.Dir(testPath)
 			if err := os.MkdirAll(dir, 0o700); err != nil {
 				t.Fatal(err)
 			}
-			if err := ioutil.WriteFile(realPath, nil, 0o600); err != nil {
+			if err := ioutil.WriteFile(testPath, nil, 0o600); err != nil {
 				t.Fatal(err)
 			}
 		}
+
+		if prefix == "" {
+			args[i+1] = testPath
+		} else {
+			args[i] = prefix + testPath
+		}
 	}
 
-	substitution := "$1" + filepath.Join(tmpDir, "$2")
-	return re.ReplaceAllString(rule, substitution)
+	return shellquote.Join(args...)
 }
 
 func deleteRules(t testing.TB, client *libaudit.AuditClient) {

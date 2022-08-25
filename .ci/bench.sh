790ab118a5f6f8049f4bd444bcecec143cd87053
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-08-25 17:01:50 +0100

    add tee

--- .ci/bench.sh
+++ .ci/bench.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 go get -d -t ./...
 go mod download
 
-go test -count=5 -benchmem -run=XXX -benchtime=100ms -bench='.*' -v $(go list ./... | grep -v /vendor/)
+go test -count=5 -benchmem -run=XXX -benchtime=100ms -bench='.*' -v $(go list ./... | grep -v /vendor/) | tee bench.out

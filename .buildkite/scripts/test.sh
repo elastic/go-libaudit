85ff3a1087462b96f1b6061fd6c4c1a39cc30ff6
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-06-28 15:57:59 +0200

    move tee

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -28,12 +28,12 @@ mkdir -p build
 go install github.com/jstemmer/go-junit-report@latest
 export OUT_FILE="build/test-report.out"
 command="$(go list ./... | grep -v /vendor/)"
-su -c "go test ${command//$'\n'/' '} | tee ${OUT_FILE}" testuser
+su -c "go test ${command//$'\n'/' '}" testuser | tee ${OUT_FILE}
 status=$?
 go-junit-report > "build/junit.xml" < ${OUT_FILE}
 
 OUT_FILE="build/test-report-386.out"
-su -c "GOARCH=386 go test ${command//$'\n'/' '} | tee ${OUT_FILE}" testuser
+su -c "GOARCH=386 go test ${command//$'\n'/' '}" testuser | tee ${OUT_FILE}
 # GOARCH=386 go test $(go list ./... | grep -v /vendor/) | tee ${OUT_FILE}
 if [ $? -gt 0 ] ; then
     status=1
